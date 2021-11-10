509b4f8ee119b5884e56b62b1d46a44687310af7
eomjieun <djawldms@gmail.com>
2021-11-10 15:38:06 +0900

    step2

---
+++ 00_ssh.tf
@@ -0,0 +1,5 @@
+resource "aws_key_pair" "jieun_key" {
+  key_name = "jieun1-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCuLOj7VKmpgCuEkhBYhby5lxPb0kJ4+uL6dVa3yEVdWyoMXq8t0T7jgLu4I7chjF19PiLkRPI/T/AwrLJGgddbVBHcM7iYllbbz5c1IhaF/CO9CoFam+oeKZ89BvCHn8cl7G7eyhftKsj7e3TzYnemnYUnn83NSfCK0XvX3UWM3qacThsAklHXV2fpDal8bnUbvpb5J1H/K/sWfCKAI7DUmUkS6rKPCQkfHV6Wucxh9HpnY0+xGmVJ+L/gQdXkKaUgy30m1bIa/ZD+Dp2XB0KAdI0twrDJWAPgjADaVW1k4GA3wdcDupl0jkYXL8k2bMiXpaFvWICG/TR8VRbouPJeaOBSvXqfQrdd0bQJs65m/W2yryhlapWW2D0dOpMW1f+73bQlkHYYLVMk4XRFACze23i4bJw3IE9wPI9vKPa8vwYPqc/nihi+zEQ6xLrWpIxMFaDEn9nEhFAi6rbyCmGr+PsZdphgB3/vLqIcBG+F+PSZj5eY5eh2WD1vUyuMDWU="
+  
+}
