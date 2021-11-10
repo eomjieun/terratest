5f49e95975af187cc40509e239fe7c9f78610d00
eomjieun <djawldms@gmail.com>
2021-11-10 17:45:39 +0900

    step1

--- 05_rt.tf
+++ 05_rt.tf
@@ -1,11 +1,11 @@
-resource "aws_route_table" "jieun_rf {
-  vpc_id = aws_vpc.jieun_vpc.id
-  
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.jieun_ig.id
+resource  "aws_route_table"  "jieun_rt"  {
+  vpc_id  =  aws_vpc.jieun_vpc.id
+
+  route  {
+    cidr_block  =  "0.0.0.0/0"
+    gateway_id  =  aws_internet_gateway.jieun_ig.id
   }
-  tags = {
-    "Name" = "jieun-rt"
+  tags  =  {
+    "Name"  =  "jieun-rt"
   }
 }
