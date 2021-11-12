ce7490e0f354f9cb0df3a76dba1aa917f394b178
eomjieun <djawldms@gmail.com>
2021-11-12 11:49:48 +0900

    step1

--- 02_vpc.tf
+++ 02_vpc.tf
@@ -1,6 +1,6 @@
-resource  "aws_vpc" "jieun_vpc" {
-    cidr_block  = "10.0.0.0/16"
-    tags = {
-      "Name" = "jieun-vpc"
-    }
+resource  "aws_vpc" "SUUUU" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "suuu-vpc"
+  }
 }
