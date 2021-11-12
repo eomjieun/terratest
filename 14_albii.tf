36c3eba95c2ad9fbdd27014128898924fffc30e9
eomjieun <djawldms@gmail.com>
2021-11-12 12:37:06 +0900

    step1

---
+++ 14_albii.tf
@@ -0,0 +1,11 @@
+resource  "aws_lb_listener"  "SUUUU_albli"  {
+  load_balancer_arn  =  aws_lb.suuu_alb.arn
+  port  =  "80"
+  protocol  =  "HTTP"
+
+
+default_action  {
+  type   =  "forward"
+  target_group_arn  =  aws_lb_target_group.SUUUU-albtg.arn
+  }
+}  
